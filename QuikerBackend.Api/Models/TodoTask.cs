bf17c6ee49b3fbdfdcc6337a41bd2cb107926528
Dawid Sibiński <dawidsibinski@gmail.com>
2023-06-15 19:36:18 +0400

    Dummy tasks controller added

---
+++ QuikerBackend.Api/Models/TodoTask.cs
@@ -0,0 +1,15 @@
+﻿namespace QuikerBackend.Api.Models
+{
+    public class TodoTask
+    {
+        public TodoTask(Guid id, string name, DateTime dueDate)
+        {
+            Id = id;
+            Name = name;
+            DueDate = dueDate;
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
