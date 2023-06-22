9e412c3c19d08e447fa0629dfca726354f72c37f
Dawid Sibiński <dawidsibinski@gmail.com>
2023-06-22 18:41:03 +0400

    Husky.NET added for pre-commit hook

--- QuikerBackend.Api/Controllers/TasksController.cs
+++ QuikerBackend.Api/Controllers/TasksController.cs
@@ -7,15 +7,16 @@ namespace QuikerBackend.Api.Controllers;
 [Route("[controller]")]
 public class TasksController : ControllerBase
 {
-    private static TodoTask[] TestTasks => new[]
-    {
-        new TodoTask(Guid.NewGuid(), "Write some code", DateTime.UtcNow.AddDays(2)),
-        new TodoTask(Guid.NewGuid(), "Set up GitHub", DateTime.UtcNow.AddDays(4)),
-        new TodoTask(Guid.NewGuid(), "Call mum for birthday", DateTime.UtcNow.AddDays(12)),
-        new TodoTask(Guid.NewGuid(), "Top up your mobile", DateTime.UtcNow),
-        new TodoTask(Guid.NewGuid(), "Finish this project :)", DateTime.UtcNow.AddDays(14)),
-        new TodoTask(Guid.NewGuid(), "Create some real data here!", DateTime.UtcNow.AddDays(22))
-    };
+    private static TodoTask[] TestTasks =>
+        new[]
+        {
+            new TodoTask(Guid.NewGuid(), "Write some code", DateTime.UtcNow.AddDays(2)),
+            new TodoTask(Guid.NewGuid(), "Set up GitHub", DateTime.UtcNow.AddDays(4)),
+            new TodoTask(Guid.NewGuid(), "Call mum for birthday", DateTime.UtcNow.AddDays(12)),
+            new TodoTask(Guid.NewGuid(), "Top up your mobile", DateTime.UtcNow),
+            new TodoTask(Guid.NewGuid(), "Finish this project :)", DateTime.UtcNow.AddDays(14)),
+            new TodoTask(Guid.NewGuid(), "Create some real data here!", DateTime.UtcNow.AddDays(22))
+        };
 
     [HttpGet(nameof(All))]
     public IEnumerable<TodoTask> All()
